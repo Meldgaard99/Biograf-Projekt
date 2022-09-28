39e81a3b5bff83536c95726b4caeadf020f5472b
morten-lund <morten2608@gmail.com>
2022-09-28 11:31:03 +0200

    test

---
+++ kunde.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+// Class
+    public class Kunde
+    {
+
+    public string Navn { get; set; }
+    public string Adresse { get; set; }
+    public int Telefon { get; set; }
+    public string Email { get; set; }
+    public List<KundeReservation> AntalReservationer = new List<KundeReservation>(); // Virker ikke?
+
+
+// Constructor
+        public Kunde(string navn = "", string adresse = "", int telefon = 0, string email = "" )
+        {
+
+        Navn = navn;
+        Adresse = adresse;
+        Telefon = telefon;
+        Email = email;
+     
+
+        }
+
+    // Metoder
+
+    public override string ToString()
+    {
+        return $" Navn = {Navn}, Adresse = {Adresse}, Telefon = {Telefon}, Email = {Email}"; 
+
+    }
+
+
+
+
+}
+
+
---
+++ kundeReservation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+//Class
+internal class KundeReservation: Kunde 
+    {
+
+    public int SalNr { get; set; }
+    public int AntalPladserReserveret { get; set; }
+    public DateTime ReservationDato { get; set; }
+    public int ArrangementID { get; set; }
+
+
+// Constructor
+// Mangler at lave constructor til datetime
+    public KundeReservation( int salNr = 0, int antalPladserReserveret = 0, int arrangementID = 0 )
+    {
+
+        SalNr = salNr;
+        AntalPladserReserveret = antalPladserReserveret;
+        ArrangementID = arrangementID;
+
+    }
+
+
+    // Metoder
+
+
+
+
+
+}
+
+
