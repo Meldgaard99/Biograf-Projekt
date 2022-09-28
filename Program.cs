40e6c19ababe0be63fabca2a96201be465f3906d
Mads Lajer <eaamalr@students.eaaa.dk>
2022-09-28 12:47:33 +0200

    Fixed reserve logic

--- Program.cs
+++ Program.cs
@@ -2,19 +2,22 @@
 
 
 
-Sal sal1 = new Sal(maxRækker: 10, maxSæder: 5);
+Sal sal1 = new Sal(maxRækker: 2, maxSæder: 4);
 Sal sal2 = new Sal(maxRækker: 5, maxSæder: 10);
 Sal sal3 = new Sal(maxRækker: 10, maxSæder: 10);
 
 Console.WriteLine("Vælg række og sæde");
-int valgtRække = int.Parse(Console.ReadLine());
-int valgtSæde = int.Parse(Console.ReadLine());
+int brugerValgtRække = int.Parse(Console.ReadLine());
+int brugerValgtSæde = int.Parse(Console.ReadLine());
 
-Console.WriteLine($"Række: {valgtRække}, Sæde: {valgtSæde}");
-
-
+Console.WriteLine($"Række: {brugerValgtRække}, Sæde: {brugerValgtSæde}");
 
 Arrangement arrangement1 = new Arrangement(sal1);
+
+KundeReservation kundeReservation1 = new KundeReservation(arrangement: arrangement1, valgtRække: brugerValgtRække, valgtSæde: brugerValgtSæde);
+
+
+
 
 
 foreach (Sæde s in arrangement1.Sæder)
--- kundeReservation.cs
+++ kundeReservation.cs
@@ -9,23 +9,35 @@ internal class KundeReservation: Kunde
     public DateTime ReservationDato { get; set; }
     public int ArrangementID { get; set; }
 
+    public int ValgtSæde { get; set; }
+
+    public int ValgtRække { get; set; }
+
+
 
 // Constructor
 // Mangler at lave constructor til datetime
-    public KundeReservation( int salNr = 0, int antalPladserReserveret = 0, int arrangementID = 0 )
+    public KundeReservation(Arrangement arrangement, int salNr = 0, int antalPladserReserveret = 0, int arrangementID = 0 , int valgtSæde = 0, int valgtRække = 0)
     {
-
+        this.ValgtSæde = valgtSæde;
+        this.ValgtRække = valgtRække; 
         SalNr = salNr;
         AntalPladserReserveret = antalPladserReserveret;
         ArrangementID = arrangementID;
-        //ReserverSæder(Arrangement);
+        ReserverSæder(arrangement);
 
     }
 
-    /*public void ReserverSæder(Arrangement arrangement)
+    public void ReserverSæder(Arrangement arrangement)
     {
-
-    }*/ 
+        foreach(Sæde s in arrangement.Sæder)
+        {
+            if(ValgtSæde == s.SædeNummer && ValgtRække == s.RækkeNummer)
+            {
+                s.SeatIsReserved = true;
+            }
+        }
+    }
 
 
     // Metoder
