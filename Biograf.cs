5a334841cf6ecb214fcd64238b94ca2611550b7f
Meldgaard99 <100026274+Meldgaard99@users.noreply.github.com>
2022-09-28 10:48:39 +0200

    Add files via upload

---
+++ Biograf.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+public class Biograf
+{
+    public string Navn { get; set; }
+
+
+    List<Sal> Autoværksted = new List<Sal>();
+    List<kunde> Autoværksted2 = new List<Kunde>();
+    List<Arrangement> Autoværksted3 = new List<Arrangement>();
+
+    f
+    public Biograf(string Navn = "")
+    {
+        Navn = Navn;
+
+    }
+
+
+    override public String ToString()
+    {
+        return $" Navn = {Navn}";
+    }
+
+}
+
+
+
+
---
+++ Sal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+    public class Sal 
+    {
+        public int MaxRækker { get; set; }
+        public int MaxSæder  { get; set; }
+    
+
+
+
+    public Sal(int maxRækker = 0, int maxSæder = 0)
+        {
+        MaxRækker = maxRækker;
+        MaxSæder = maxSæder;
+            
+        }
+
+         
+
+    override public String ToString()
+        {
+            return $" MaxRækker = {MaxRækker}, MaxSæder = {MaxSæder},Reserverepladser = reserverPladser(), salFuld() ";
+        }
+
+    }
+
+
+
+
